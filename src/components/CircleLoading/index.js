4d515af2badf70fbe89243ca2ec77fc5dc3bba6b
Devansh <devanshsoni811@gmail.com>
2023-04-24 04:31:07 +0530

    API URL Update

---
+++ src/components/CircleLoading/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CircleLoading.css";
+
+const index = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default index;
