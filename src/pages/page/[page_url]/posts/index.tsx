14bd1bfd2f1a6f3352f1d68ea4e91a859f95a43d
Ashish558 <ashishprasad2001ap@gmail.com>
2023-05-24 12:12:43 +0530

    listing page ui change

---
+++ src/pages/page/[page_url]/posts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = { data: any }
+
+const Posts: React.FC<Props> = (props) => {
+  
+
+   return (
+      <>
+         
+      </>
+   )
+}
+
+export default Posts
