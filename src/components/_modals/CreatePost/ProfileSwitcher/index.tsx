cb0fcc35142fadfc508c6937cb52a866ee34cc53
Devansh <devanshsoni811@gmail.com>
2023-05-26 19:25:28 +0530

    Profile Switcher BUg Fixes

---
+++ src/components/_modals/CreatePost/ProfileSwitcher/index.tsx
@@ -0,0 +1,118 @@
+import React, { useRef, useState } from 'react'
+import styles from './styles.module.css'
+import Image from 'next/image'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/redux/store'
+import useOutsideAlerter from '@/hooks/useOutsideAlerter'
+import { DEFAULT_PROFILE_IMAGES } from '@/utils'
+import { updateActiveProfile } from '@/redux/slices/user'
+
+type Props = {
+  data: any
+  setData: any
+}
+
+const ProfileSwitcher: React.FC<Props> = ({ data: activeProfile, setData }) => {
+  const { user, listing } = useSelector((state: RootState) => state.user)
+
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+  const dropdownRef = useRef(null)
+
+  useOutsideAlerter(dropdownRef, () => setShowDropdown(false))
+
+  const handleUpdateActiveProfile = (type: 'user' | 'listing', data: any) => {
+    setData((prev: any) => {
+      return { ...prev, type, data }
+    })
+    setShowDropdown(false)
+  }
+
+  return (
+    <>
+      <section
+        className={`content-box-wrapper ${styles['profile-switcher']} ${
+          showDropdown ? styles['show-dropdown'] : ''
+        }`}
+        ref={dropdownRef}
+        onClick={() => setShowDropdown((prev) => !prev)}
+      >
+        <Image
+          data-profile-type={activeProfile.type}
+          src={activeProfile.data?.profile_image || DEFAULT_PROFILE_IMAGES.user}
+          alt=""
+          width={40}
+          height={40}
+        />
+        <p className={styles['name']}>
+          {activeProfile.type === 'listing'
+            ? activeProfile.data.title
+            : activeProfile.data?.full_name}
+        </p>
+
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <g clip-path="url(#clip0_25_51286)">
+            <path
+              d="M15.88 9.29055L12 13.1705L8.11998 9.29055C7.72998 8.90055 7.09998 8.90055 6.70998 9.29055C6.31998 9.68055 6.31998 10.3105 6.70998 10.7005L11.3 15.2905C11.69 15.6805 12.32 15.6805 12.71 15.2905L17.3 10.7005C17.69 10.3105 17.69 9.68055 17.3 9.29055C16.91 8.91055 16.27 8.90055 15.88 9.29055Z"
+              fill="#08090A"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_25_51286">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+
+        {showDropdown && (
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className={`${styles['dropdown']} `}
+          >
+            <ul className={styles['dd-list']}>
+              <li
+                onClick={() => handleUpdateActiveProfile('user', user)}
+                className={`${styles['dd-item']} ${
+                  activeProfile.type === 'user' && styles['active']
+                }`}
+              >
+                <Image
+                  src={user?.profile_image || DEFAULT_PROFILE_IMAGES.user}
+                  alt=""
+                  width={24}
+                  height={24}
+                  data-profile-type="user"
+                />
+                <p>{user.full_name}</p>
+              </li>
+
+              {listing.map((page: any) => {
+                return (
+                  <li
+                    key={page._id}
+                    onClick={() => handleUpdateActiveProfile('listing', page)}
+                    className={`${styles['dd-item']} ${
+                      activeProfile.type === 'listing' &&
+                      activeProfile.data._id === page._id &&
+                      styles['active']
+                    }`}
+                  >
+                    <Image
+                      src={page?.profile_image || DEFAULT_PROFILE_IMAGES.user}
+                      alt=""
+                      width={24}
+                      height={24}
+                      data-profile-type="listing"
+                    />
+                    <p>{page.title}</p>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
+      </section>
+    </>
+  )
+}
+
+export default ProfileSwitcher
