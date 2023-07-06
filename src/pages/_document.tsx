a9b63271cd457671a9cb4776d2ba9cae60841d94
Ashish558 <ashishprasad2001ap@gmail.com>
2023-07-06 17:23:33 +0530

    hobby modal ui bug fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+          <link rel="icon" type="image/x-icon" href="/logo-small.png" />
+
       </Head>
       <body>
         <Main />
