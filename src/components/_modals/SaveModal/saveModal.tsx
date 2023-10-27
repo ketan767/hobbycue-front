84d4190c4cd249015d72bf02faa16275893e9cae
Ashish558 <ashishprasad2001ap@gmail.com>
2023-10-27 13:55:58 +0530

    feat:save functionality added

---
+++ src/components/_modals/SaveModal/saveModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './saveModal.module.css'
+import FilledButton from '@/components/_buttons/FilledButton'
+import OutlinedButton from '@/components/_buttons/OutlinedButton'
+
+type Props = {
+  setConfirmationModal?: any
+  handleClose?: any
+  handleSubmit?: any
+}
+
+const SaveModal: React.FC<Props> = ({
+  setConfirmationModal,
+  handleClose,
+  handleSubmit,
+}) => {
+  return (
+    <div className={`${styles['confirmation-modal']}`}>
+      <div className={styles['confirmation-modal-body']}>
+        <p> Would you like to save before exits ? </p>
+        <div className={styles['buttons']}>
+          <FilledButton className={styles['button1']} onClick={handleSubmit}>
+            Yes
+          </FilledButton>
+          <OutlinedButton
+            onClick={() => {
+              handleClose()
+              setConfirmationModal(false)
+            }}
+          >
+            No
+          </OutlinedButton>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SaveModal
