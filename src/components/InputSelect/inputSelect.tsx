df99854284816d46ae8f57982be61f4eb3864f0e
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-11-17 14:48:13 +0530

    settings->localization & payments-> dropdowns data added, phone prefix default based on redion

--- src/components/InputSelect/inputSelect.tsx
+++ src/components/InputSelect/inputSelect.tsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './input.module.css'
 
 type Props = {
-   options: any
-   onChange? : any
-   value? : any
+  options: any
+  onChange?: any
+  value?: any
+  name?: any
 }
 
-const InputSelect: React.FC<Props> = ({  options, onChange, value }) => {
-
-   return (
-      <select name='select' className={styles.select} onChange={(e:any) => onChange(e.target.value)} value={value} >
-         {options.map((item: any) => {
-            return <option key={item} className={styles.option}>
-               {item}
-            </option>
-         })}
-      </select>
-
-   )
+const InputSelect: React.FC<Props> = ({ options, onChange, value, name }) => {
+  const [selectWidth, setSelectWidth] = useState((value.length*10+50)+'px')
+  const handleChange = (option:string) => {
+   let length=(option.length*10+50)+'px'
+   setSelectWidth(length)
+  }
+  return (
+    <select
+      name={name}
+      className={styles.select}
+      onChange={(e: any) => {
+        onChange({ name: e.target.name, value: e.target.value })
+        handleChange(e.target.value)
+      }}
+      value={value}
+      style={{width:selectWidth}}
+    >
+      {options.map((item: any) => {
+        return (
+          <option key={item} className={styles.option}>
+            {item}
+          </option>
+        )
+      })}
+    </select>
+  )
 }
 
 export default InputSelect
