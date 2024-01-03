c97065b548a1a81cd472f4cd1155297ed3709d8f
Ketan <kp315884@gmail.com>
2024-01-03 17:09:45 +0530

    working on SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'http://localhost:3000', // Replace with your actual site URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+  // Other configuration options...
+}
