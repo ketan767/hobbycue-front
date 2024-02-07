0fccbb252a2cbc84e1364e0747320beec21285bf
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-02-08 02:02:34 +0530

    Multiple small issues fixed

---
+++ src/components/PostCard/CommentCheckWithUrl.tsx
@@ -0,0 +1,26 @@
+import { FC, useEffect, useState } from 'react'
+import styles from './PostCard.module.css'
+
+interface CommentCheckWithUrlProps {
+  children: React.ReactNode
+}
+
+const CommentCheckWithUrl: FC<CommentCheckWithUrlProps> = ({ children }) => {
+  const [url, setUrl] = useState<null | string>(null)
+  const pageUrlClass = styles.postUrl
+  useEffect(() => {
+    const checkUrl = () => {
+      const regex =
+        /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/
+      const url = children?.toString().match(regex)
+      if (url) {
+        setUrl(url[0])
+      }
+    }
+    checkUrl()
+  }, [children])
+  if (typeof url === 'string') return <a href={url} className={pageUrlClass} target="_blank">{children}</a>
+  else return <p className={styles['content']}>{children}</p>
+}
+
+export default CommentCheckWithUrl
