fe42f487b33e282cef8a6ce77b077a8ecef366a5
Ketan <kp315884@gmail.com>
2024-05-30 19:22:41 +0530

    open admin only when user is admin

--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,6 +1,7 @@
 import AdminNavbar from '@/components/AdminNavbar/AdminNavbar'
 import { RootState } from '@/redux/store'
-import React, { FC } from 'react'
+import { useRouter } from 'next/router'
+import React, { FC, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 interface AdminLayoutProps {
@@ -8,7 +9,20 @@ interface AdminLayoutProps {
 }
 
 const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
+  const router = useRouter()
+  const { user } = useSelector((state: RootState) => state.user)
   const { admin_nav } = useSelector((state: RootState) => state.site)
+
+  useEffect(() => {
+    if (!user.is_admin) {
+      router.push('https://hobbycue.com/')
+    }
+  }, [user, router])
+
+  if (!user.is_admin) {
+    return null // Optionally render a loading state or null
+  }
+
   return (
     <>
       <AdminNavbar />
@@ -17,7 +31,7 @@ const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
           style: {
             width: `calc(100% - ${admin_nav ? '248px' : '59px'})`,
             left: admin_nav ? '248px' : '59px',
-            position:"relative"
+            position: 'relative',
           },
         }),
       )}
