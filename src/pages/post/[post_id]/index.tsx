96d3736738915556f30a7355d7d61f22b4414808
Ketan <kp315884@gmail.com>
2024-06-19 01:43:20 +0530

    testing post preview 1

--- src/pages/post/[post_id]/index.tsx
+++ src/pages/post/[post_id]/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
 import PageContentBox from '@/layouts/PageContentBox'
 import PageGridLayout from '@/layouts/PageGridLayout'
 import { withAuth } from '@/navigation/withAuth'
@@ -9,7 +10,6 @@ import store, { RootState } from '@/redux/store'
 import EditIcon from '@/assets/svg/edit-icon.svg'
 import { openModal } from '@/redux/slices/modal'
 import { getAllPosts } from '@/services/post.service'
-import { GetServerSideProps } from 'next'
 import post, { setActivePost, updatePosts } from '@/redux/slices/post'
 import PostCard from '@/components/PostCard/PostCard'
 import ProfileSwitcher from '@/components/ProfileSwitcher/ProfileSwitcher'
@@ -19,18 +19,32 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import CommunityPageLayout from '@/layouts/CommunityPageLayout'
 import { setShowPageLoader } from '@/redux/slices/site'
+import { getAllUserDetail } from '@/services/user.service'
+import { getListingPages } from '@/services/listing.service'
 
-type Props = {}
+type Props = {
+  data: {
+    pageData: any
+    postsData: any
+    mediaData: any
+    listingsData: any
+    blogsData: any
+  }
+}
 
-const CommunityLayout: React.FC<Props> = ({}) => {
+const CommunityLayout: React.FC<Props> = ({ data }) => {
   const router = useRouter()
   const [postId, setPostId] = useState<string | null>(null)
+  const [postData, setPostData] = useState<any>(null)
+  const [isLoadingPosts, setIsLoadingPosts] = useState(false)
+  const dispatch = useDispatch()
 
   const getLastIdFromUrl = (url: any) => {
     const urlObj = new URL(url)
     const paths = urlObj.pathname.split('/')
     return paths[paths.length - 1]
   }
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const currUrl = window.location.href
@@ -43,14 +57,12 @@ const CommunityLayout: React.FC<Props> = ({}) => {
   const { activeProfile } = useSelector((state: RootState) => state.user)
   const { allPosts } = useSelector((state: RootState) => state.post)
 
-  const [isLoadingPosts, setIsLoadingPosts] = useState(false)
-  const [postData, setPostData] = useState<any>(null)
-  const dispatch = useDispatch()
   const getPost = async () => {
+    if (!postId) return
+
     const params = new URLSearchParams(
       `populate=_author,_genre,_hobby&_id=${postId}`,
     )
-
     setIsLoadingPosts(true)
     const { err, res } = await getAllPosts(params.toString())
     if (err) return console.log(err)
@@ -60,27 +72,49 @@ const CommunityLayout: React.FC<Props> = ({}) => {
     }
     setIsLoadingPosts(false)
   }
+
   const openPostmodal = () => {
-    dispatch(setActivePost(postData))
-    dispatch(openModal({ type: 'post', closable: false }))
+    if (postData) {
+      dispatch(setActivePost(postData))
+      dispatch(openModal({ type: 'post', closable: false }))
+    }
   }
 
   useEffect(() => {
     if (postId) {
       getPost()
     }
-  }, [activeProfile, postId])
+  }, [postId])
+
   useEffect(() => {
     if (postData?.content) openPostmodal()
-  }, [[postData?.content]])
+  }, [postData?.content])
+
   return (
     <>
+      <Head>
+        <title>{postData ? postData.title : 'Loading...'}</title>
+        <meta
+          property="og:title"
+          content={postData ? postData.title : 'Loading...'}
+        />
+        <meta
+          property="og:description"
+          content={postData ? postData.description : 'Description'}
+        />
+        <meta
+          property="og:image"
+          content={postData ? postData.image : '/default-image.png'}
+        />
+        <meta
+          property="og:url"
+          content={typeof window !== 'undefined' ? window.location.href : ''}
+        />
+      </Head>
       <CommunityPageLayout activeTab="posts" singlePostPage={true}>
         <main>
           {!postData || isLoadingPosts ? (
-            <>
-              <PostCardSkeletonLoading />
-            </>
+            <PostCardSkeletonLoading />
           ) : (
             <PostCard postData={postData} />
           )}
