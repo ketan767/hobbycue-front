97e7b937732ee1eab253297d8ad4f391ca5e59fe
hemvish11 <hemvish11@gmail.com>
2024-12-03 15:31:38 +0530

    genre change should fetch data bug solved

--- src/layouts/CommunityPageLayout/index.tsx
+++ src/layouts/CommunityPageLayout/index.tsx
@@ -279,7 +279,7 @@ const CommunityLayout: React.FC<Props> = ({
   }
   useEffect(() => {
     scrollToTop()
-  }, [selectedHobby])
+  }, [selectedHobby,selectedGenre])
 
   const fetchPosts = async (page = 1) => {
     if (showPageLoader) {
@@ -520,7 +520,7 @@ const CommunityLayout: React.FC<Props> = ({
         fetchPosts()
       }
     }
-  }, [selectedHobby, selectedLocation, activeProfile?.type, refreshNum])
+  }, [selectedHobby,selectedGenre, selectedLocation, activeProfile?.type, refreshNum])
 
   useEffect(() => {
     if (selectedHobby !== '' && selectedLocation !== '') {
